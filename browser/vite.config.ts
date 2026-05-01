c01689a9ba106aa6b3be7d7f4ab8688ff113f685
Arzuparreta <rubenpenarubio81@gmail.com>
2026-05-01 21:28:59 +0200

    fix assets path

--- browser/vite.config.ts
+++ browser/vite.config.ts
@@ -3,6 +3,7 @@ import path from 'path';
 
 export default defineConfig({
   root: '.',
+  base: '/browser/',
   build: {
     outDir: 'dist',
     emptyOutDir: true
--- mobile/vite.config.ts
+++ mobile/vite.config.ts
@@ -3,6 +3,7 @@ import path from 'path';
 
 export default defineConfig({
   root: '.',
+  base: '/mobile/',
   build: {
     outDir: 'dist',
     emptyOutDir: true
